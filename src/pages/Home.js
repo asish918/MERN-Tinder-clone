27208280d4dd61fd9f134d22149a3831df3304c4
Asish Mahapatra <asishmahapatra918@gmail.com>
2022-06-15 12:03:03 +0530

    Basic Navbar and Homepage UI

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,27 @@
+import Nav from "../components/Nav";
+
 const Home = () => {
+
+    const authToken = false;
+    const minimal = false
+
+    const handleClick = () => {
+        console.log('Clicked bimches')
+    }
+
     return (
-        <div>HomePage</div>
-    )   
+        <>
+            <div className="overlay">
+                <Nav minimal={minimal} authToken={authToken} />
+                <div className="home">
+                    <h1>Swipe Right™</h1>
+                    <button className="primary-button" onClick={handleClick}>
+                        {authToken ? 'Signout' : 'Create Account'}
+                    </button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home;
